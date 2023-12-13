795a3a537eb5875a48cca3c73f3c1bcc626b4139
AbdelrhmanAmer <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>
2023-12-13 09:59:22 +0200

    Display Messages

--- chat_app/lib/widgets/chat_messages.dart
+++ chat_app/lib/widgets/chat_messages.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -5,6 +7,36 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chat')
+            .orderBy('createAt', descending: false)
+            .snapshots(),
+        builder: ((ctx, snapshot){
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }else if(!snapshot.hasData || snapshot.data == null){
+            return const Center(
+              child: Text('No Message Found..!'),
+            );
+          }else if(snapshot.hasError){
+            return const Center(
+              child: Text('Something went wrong..!'),
+            );
+          }
+
+          final List<QueryDocumentSnapshot<Map<String, dynamic>>> loadedMessages = snapshot.data!.docs;
+
+          return ListView.builder(
+              itemCount: loadedMessages.length,
+              itemBuilder: (ctx, index){
+                return Text(
+                    loadedMessages[index].data()['text']
+                );
+              });
+        })
+    );
   }
 }
